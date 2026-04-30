254c6405f911354a9b9c9b47881047dd9048929a
feiranran <669401471@qq.com>
2026-04-30 18:34:18 +0800

    debug

--- server.js
+++ server.js
@@ -57,7 +57,7 @@ app.use((req, res) => {
   });
 });
 
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log('Cream Market is running at http://localhost:' + PORT);
 });
 // end: Codex, GPT-5.5 High, OpenAI.
